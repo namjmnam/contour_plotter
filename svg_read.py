cf24ca1cef505203a9bdadf62b922d41dde3d6b3
namjmnam <namjmnam@naver.com>
2024-01-05 14:29:14 +0900

    Added on-pick events, but it's everywhere

--- svg_read.py
+++ svg_read.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from svg.path import parse_path
 from xml.dom import minidom
+import numpy as np
 
 # Function to extract paths from the SVG file
 def extract_svg_paths(svg_file):
@@ -9,25 +10,47 @@ def extract_svg_paths(svg_file):
     doc.unlink()
     return path_strings
 
-# Function to plot paths
-def plot_paths(paths):
+# Function to plot paths and add interactive points
+def plot_interactive_paths(paths):
+    fig, ax = plt.subplots()
+
+    points = []  # List to store the points
+
+    def on_pick(event):
+        # This function will be called when a point is clicked
+        point = event.artist
+        x, y = point.get_data()
+        z = float(input(f"Enter Z value for point ({x[0]}, {y[0]}): "))  # Ask user for Z value
+        point.set_color('green')  # Change color to indicate the point has a Z value set
+        points.append((x[0], y[0], z))  # Add the point and Z value to the list
+        plt.draw()
+
     for path_string in paths:
         path = parse_path(path_string)
 
         for segment in path:
-            # Assuming segment is a Line or CubicBezier, more types can be added
             if hasattr(segment, 'start') and hasattr(segment, 'end'):
                 x_values = [segment.start.real, segment.end.real]
                 y_values = [segment.start.imag, segment.end.imag]
-                plt.plot(x_values, y_values, 'b')
-            # Add more handling here for other segment types if needed
+                line, = ax.plot(x_values, y_values, 'b')
+
+                # Add clickable red dot at the start of the segment
+                red_dot, = ax.plot(segment.start.real, segment.start.imag, 'ro', picker=5, markersize=8)
+                red_dot.set_picker(5)  # 5 points tolerance
+
+    # Connect the pick event to the on_pick function
+    fig.canvas.mpl_connect('pick_event', on_pick)
 
     plt.gca().set_aspect('equal', adjustable='box')
     plt.show()
 
+    return points
+
 # SVG file path
 svg_file = './JtossSVG1.svg'
 
-# Extract and plot paths
+# Extract and plot paths interactively
 paths = extract_svg_paths(svg_file)
-plot_paths(paths)
+points_with_z = plot_interactive_paths(paths)
+
+# You can now use points_with_z for further processing
