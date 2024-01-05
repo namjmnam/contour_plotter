864932547303f644ac45224c4f29cd29759b5a57
namjmnam <namjmnam@naver.com>
2024-01-05 15:01:05 +0900

    Wrong curves. Need to be reverted

--- svg_read.py
+++ svg_read.py
@@ -4,6 +4,8 @@ from svg.path import parse_path
 from xml.dom import minidom
 import tkinter as tk
 from tkinter import simpledialog
+from scipy.interpolate import CubicSpline
+import numpy as np
 
 # Initialize Tkinter root - needed for dialog
 tk_root = tk.Tk()
@@ -16,17 +18,17 @@ def extract_svg_paths(svg_file):
     doc.unlink()
     return path_strings
 
+
 # Function to plot paths and add interactive points
 def plot_interactive_paths(paths):
     fig, ax = plt.subplots()
 
-    path_data = []  # List to store path data and their Z values
+    path_data = []
 
     def on_pick(event):
-        # This function will be called when a path point is clicked
         path_index = event.artist.get_gid()
         z = simpledialog.askfloat("Input", f"Enter Z value for path {path_index}:", parent=tk_root)
-        if z is not None:  # Check if the user entered a value
+        if z is not None:
             path_data[path_index]['z'] = z
 
     for index, path_string in enumerate(paths):
@@ -38,17 +40,20 @@ def plot_interactive_paths(paths):
                 x_values.extend([segment.start.real, segment.end.real])
                 y_values.extend([segment.start.imag, segment.end.imag])
 
-        # Plot the path
-        ax.plot(x_values, y_values, 'b')
+        # Apply spline interpolation
+        x_values, y_values = np.array(x_values), np.array(y_values)
+        cs_x = CubicSpline(range(len(x_values)), x_values)
+        cs_y = CubicSpline(range(len(y_values)), y_values)
+        x_smooth = np.linspace(0, len(x_values) - 1, 300)
+        y_smooth = cs_y(x_smooth)
 
-        # Add a clickable point for the path
+        ax.plot(cs_x(x_smooth), y_smooth, 'b')
+
         clickable_point, = ax.plot(x_values[0], y_values[0], 'ro', picker=5, markersize=8, gid=index)
-        clickable_point.set_picker(5)  # 5 points tolerance
+        clickable_point.set_picker(5)
 
-        # Store the path data
-        path_data.append({'x': x_values, 'y': y_values, 'z': 0})
+        path_data.append({'x': cs_x(x_smooth), 'y': y_smooth, 'z': 0})
 
-    # Connect the pick event to the on_pick function
     fig.canvas.mpl_connect('pick_event', on_pick)
 
     plt.gca().set_aspect('equal', adjustable='box')
@@ -69,7 +74,7 @@ def plot_3d_paths(path_data):
     plt.show()
 
 # SVG file path
-svg_file = './JtossSVG1.svg'
+svg_file = './p5oil.svg'
 
 # Extract and plot paths interactively
 path_data = plot_interactive_paths(extract_svg_paths(svg_file))
