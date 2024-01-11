Fixed the issue of scaling

interpolator.py
@@ -132,10 +132,10 @@ grid_x, grid_y = np.meshgrid(
 grid_z = griddata((x, y), z, (grid_x, grid_y), method='cubic')
 
 # Set z values to 0 for points outside the "outer" contour or inside the "inner" contour
-for i in range(grid_z.shape[0]):
-    for j in range(grid_z.shape[1]):
-        if not is_point_inside_path(grid_x[i, j], grid_y[i, j], outer) or is_point_inside_path(grid_x[i, j], grid_y[i, j], inner):
-            grid_z[i, j] = np.nan
+# for i in range(grid_z.shape[0]):
+#     for j in range(grid_z.shape[1]):
+#         if not is_point_inside_path(grid_x[i, j], grid_y[i, j], outer) or is_point_inside_path(grid_x[i, j], grid_y[i, j], inner):
+#             grid_z[i, j] = np.nan
 
 # Plotting 1
 plt.figure(figsize=(10, 8))

pixel_collector.py
@@ -5,6 +5,9 @@ from scipy.interpolate import interp1d
 import os
 import pandas as pd
 
+# Divide by 8 by default
+scale = 8
+
 def pixel_counter(image_path):
     # Open the image
     with Image.open(image_path) as img:
@@ -34,7 +37,7 @@ def pixel_counter(image_path):
                         black_pixel_counts[x] += 1
                 else:
                     raise ValueError(f"Unsupported image mode: {mode}")
-        return_list = [x/8 for x in black_pixel_counts] # Assume 8 pixels = 1m
+        return_list = [x/scale for x in black_pixel_counts] # Assume 8 pixels = 1m
         return return_list
 
 def get_all_pics(folder_path):
@@ -132,7 +135,7 @@ def main():
         full_pixels+=segment
         interpolated_segment = interpolate_and_sample(segment, 11, plot_type='line')
         full_interpolated+=list(interpolated_segment)[:-1] # Remove overlapping element from the last of the list
-    full_interpolated+=[51.0] # Re-add the last element of the last segment, which will always be 51
+    full_interpolated+=[51.0/scale] # Re-add the last element of the last segment, which will always be 51 (and divide by 8)
 
     # plot_data(full_pixels, plot_type='line')
     # plot_data(full_interpolated, plot_type='line')